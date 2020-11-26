93147f837d6c59783cd73adba04a50ab32bf4d33
[Your <[Your>
2020-11-27 00:15:05 +0200

    Custom Health implementation

---
+++ src/main/java/ua/dnipro/epam/bohdan/homework_5/CustomHealthCheck.java
@@ -0,0 +1,26 @@
+package ua.dnipro.epam.bohdan.homework_5;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class CustomHealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        double chance = ThreadLocalRandom.current().nextDouble();
+        Health.Builder status = Health.up();
+        if (chance > 0.9) {
+            status = Health.down();
+        }
+
+        return status
+                .withDetail("nameApp", "Tour Agency")
+                .withDetail("homework", "5")
+                .build();
+    }
+
+}
