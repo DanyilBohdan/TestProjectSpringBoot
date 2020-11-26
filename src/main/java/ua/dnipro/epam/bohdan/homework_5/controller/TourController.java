c69b38b969e17a2f1e5d8481aaacb8f11535d493
[Your <[Your>
2020-11-26 23:17:00 +0200

    Pagination and sorting implementation

--- src/main/java/ua/dnipro/epam/bohdan/homework_5/controller/TourController.java
+++ src/main/java/ua/dnipro/epam/bohdan/homework_5/controller/TourController.java
@@ -2,6 +2,8 @@ package ua.dnipro.epam.bohdan.homework_5.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +20,25 @@ public class TourController {
     @Autowired
     private TourRepository tourRepository;
 
-    @GetMapping(value = "/all")
-    public Iterable<Tour> getAllTour(){
-        return tourRepository.findAll();
+//    @GetMapping(value = "/all")
+//    public Iterable<Tour> getAllTour(){
+//        return tourRepository.findAll();
+//    }
+
+    @GetMapping(value = "/all/{page}/{sort}")
+    public Iterable<Tour> getAllTourSort(@PathVariable Integer page,
+                                         @PathVariable String sort) {
+        return tourRepository.findAll(PageRequest.of(page - 1, 4, Sort.by(sort)));
+    }
+
+    @GetMapping(value = "/all/{page}/{sort}/desc")
+    public Iterable<Tour> getAllTourSortDesc(@PathVariable Integer page,
+                                             @PathVariable String sort) {
+        return tourRepository.findAll(PageRequest.of(page - 1, 4, Sort.by(sort).descending()));
     }
 
     @GetMapping(value = "/{id}")
-    public Optional<Tour> getAllById(@PathVariable Long id){
+    public Optional<Tour> getAllById(@PathVariable Long id) {
         return tourRepository.findById(id);
     }
 
--- src/main/java/ua/dnipro/epam/bohdan/homework_5/repository/TourRepository.java
+++ src/main/java/ua/dnipro/epam/bohdan/homework_5/repository/TourRepository.java
@@ -1,15 +1,19 @@
 package ua.dnipro.epam.bohdan.homework_5.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ua.dnipro.epam.bohdan.homework_5.entity.Tour;
 
 import java.util.Optional;
 
-public interface TourRepository extends CrudRepository<Tour, Long> {
+public interface TourRepository extends CrudRepository<Tour, Long>, PagingAndSortingRepository<Tour, Long> {
 
     @Override
-    Iterable<Tour> findAll();
+    Page<Tour> findAll(Pageable pageable);
 
     @Override
     Optional<Tour> findById(Long aLong);
+
 }
