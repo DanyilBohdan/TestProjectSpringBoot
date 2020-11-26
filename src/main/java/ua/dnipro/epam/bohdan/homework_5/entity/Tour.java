0d5d3f9d4e60a6e017ac1006a35ccb179a4c68d4
[Your <[Your>
2020-11-27 00:51:47 +0200

    Lombok

--- src/main/java/ua/dnipro/epam/bohdan/homework_5/entity/Tour.java
+++ src/main/java/ua/dnipro/epam/bohdan/homework_5/entity/Tour.java
@@ -1,10 +1,15 @@
 package ua.dnipro.epam.bohdan.homework_5.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 public class Tour {
 
     @Id
@@ -28,84 +33,4 @@ public class Tour {
     private Date start_date;
 
     private Integer days;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getType_tour() {
-        return type_tour;
-    }
-
-    public void setType_tour(String type_tour) {
-        this.type_tour = type_tour;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
-    }
-
-    public Integer getCount_people() {
-        return count_people;
-    }
-
-    public void setCount_people(Integer count_people) {
-        this.count_people = count_people;
-    }
-
-    public Integer getMark_hotel() {
-        return mark_hotel;
-    }
-
-    public void setMark_hotel(Integer mark_hotel) {
-        this.mark_hotel = mark_hotel;
-    }
-
-    public Date getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(Date start_date) {
-        this.start_date = start_date;
-    }
-
-    public Integer getDays() {
-        return days;
-    }
-
-    public void setDays(Integer days) {
-        this.days = days;
-    }
 }
